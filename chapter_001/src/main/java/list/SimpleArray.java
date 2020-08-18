d6b37a530f7e0c180f49beb69ab0827a505003d6
Rocket47 <romtswork@gmail.com>
2020-08-18 14:32:02 +0300

    The first working version of the task #158

--- chapter_001/src/main/java/list/SimpleArray.java
+++ chapter_001/src/main/java/list/SimpleArray.java
@@ -6,31 +6,47 @@ public class SimpleArray<T> implements Iterable<T> {
     int capacity = 10;
     int currentIndex = 0;
     int modCount = 0;
-    int indexIterator = 0;
+
     Object[] container = new Object[capacity];
 
     public T get(int index) {
         Objects.checkIndex(index, this.currentIndex);
-        return (T) container[currentIndex];
+        return (T) container[index];
     }
 
     public void add(T model) {
-        if (container.length >= capacity) {
+        if (container.length > capacity) {
             container = Arrays.copyOf(container, capacity * 2);
         }
         container[currentIndex] = model;
         modCount++;
+        currentIndex++;
     }
 
     @Override
     public Iterator<T> iterator() {
-        int expectedModCount = modCount;
-        if (!this.iterator().hasNext()) {
-            throw new NoSuchElementException();
-        }
-        if (expectedModCount >= modCount) {
-            throw new ConcurrentModificationException();
-        }
-        return (Iterator<T>) container[indexIterator++];
+        return new Iterator<T>() {
+            int indexIterator = 0;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (indexIterator < currentIndex) {
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return (T) container[indexIterator++];
+            }
+        };
     }
 }
