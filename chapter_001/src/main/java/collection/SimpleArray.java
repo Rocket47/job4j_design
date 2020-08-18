4945f137a8bf23ba1714118ae7b91a9dc413f003
Rocket47 <romtswork@gmail.com>
2020-08-18 14:34:32 +0300

    The package list was renamed.

--- chapter_001/src/main/java/collection/SimpleArray.java
+++ chapter_001/src/main/java/collection/SimpleArray.java
@@ -1,4 +1,4 @@
-package list;
+package collection;
 
 import java.util.*;
 
--- chapter_001/src/test/java/collection/SimpleArrayTest.java
+++ chapter_001/src/test/java/collection/SimpleArrayTest.java
@@ -1,4 +1,4 @@
-package list;
+package collection;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
